31a6f8017e3724c47d79c8e99c65a5bdda3bda8c
EvilKris <66674433+EvilKris@users.noreply.github.com>
2026-03-23 12:04:53 +0900

    anim update

---
+++ Assets/GRAPHICS/SHADERS/PostProcessing/GrayScaleRendererFeature.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class GrayScaleRendererFeature : ScriptableRendererFeature
+{
+    [SerializeField]
+    private Settings settings = new Settings();
+
+    private string ShaderName => "Hidden/Post-processing Custom/GrayScale";
+    private GrayScaleRenderPass _pass;
+
+    private GrayScaleRenderPass CreatePass(Settings s)
+    {
+        if (s.shader == null)
+            s.shader = Shader.Find(ShaderName);
+
+        return new GrayScaleRenderPass(s.shader, s.injectionPoint);
+    }
+
+    public override void Create()
+    {
+        _pass = CreatePass(settings);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (renderingData.cameraData.isSceneViewCamera) return;
+        // SceneView等のフィルタ／XR判定などがあればここで
+        var v = VolumeManager.instance.stack.GetComponent<GrayScaleVolume>();
+        if (v == null || !v.IsActive())
+            return;
+
+        renderer.EnqueuePass(_pass);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (_pass != null)
+        {
+            _pass.Dispose();
+            _pass = null;
+        }
+    }
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        // 旧pass破棄
+        if (_pass != null)
+        {
+            _pass.Dispose();
+            _pass = null;
+        }
+        // 再生成
+        Create();
+    }
+#endif
+
+    [System.Serializable]
+    public class Settings
+    {
+        public Shader shader;
+        public RenderPassEvent injectionPoint = RenderPassEvent.AfterRenderingPostProcessing;
+    }
+}
---
+++ Assets/GRAPHICS/SHADERS/PostProcessing/GrayScaleVolume.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[VolumeComponentMenu("Post-processing Custom/GrayScale")]
+[VolumeRequiresRendererFeatures(typeof(GrayScaleRendererFeature))]
+public class GrayScaleVolume : VolumeComponent, IPostProcessComponent
+{
+    /// <summary>
+    /// オーバーレイ計算をかけた画像を、元画像にブレンドする係数
+    /// </summary>
+    public ClampedFloatParameter blend = new ClampedFloatParameter(0f, 0f, 1f);
+
+    public bool IsActive() => active && EffectActive();
+
+    /// <summary>
+    /// レンダリング判定
+    /// </summary>
+    private bool EffectActive() => blend.value > 0f;
+}
